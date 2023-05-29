40f5ae6f2b7f0c2b5d4d78f5498cd622b3995170
Omar Rodriguez <orodriguez@grubhub.com>
2023-05-29 18:10:52 -0400

    Sliding Windows Solution

--- LeetCodeChallenges/_3_LengthOfLongestSubstring/Tests.cs
+++ LeetCodeChallenges/_3_LengthOfLongestSubstring/Tests.cs
@@ -12,7 +12,7 @@ public class Tests
     [InlineData("pwwkew", 3)]
     public void Test(string str, int expected)
     {
-        var solution = new BruteForce();
+        var solution = new SlidingWindow();
         var result = solution.LengthOfLongestSubstring(str);
         Assert.Equal(expected, result);
     }
@@ -20,6 +20,7 @@ public class Tests
 
 public interface ISolution
 {
+    int LengthOfLongestSubstring(string str);
 }
 
 public class BruteForce : ISolution
@@ -41,7 +42,38 @@ public class BruteForce : ISolution
 
         if (!currentSubString.Contains(currentChar))
             return Subs(currentSubString.Concat(new[] { currentChar }).ToArray(), rest.Skip(1).ToArray());
-        
+
         return currentSubString;
+    }
+}
+
+public class SlidingWindow : ISolution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        var chars = new Dictionary<char, int>(0);
+
+        var left = 0;
+        var right = 0;
+
+        var res = 0;
+        while (right < s.Length)
+        {
+            var r = s[right];
+            chars[r] = chars.GetValueOrDefault(r, 0) + 1;
+
+            while (chars[r] > 1)
+            {
+                var l = s[left];
+                chars[l] -= 1;
+                left++;
+            }
+
+            res = Math.Max(res, right - left + 1);
+
+            right++;
+        }
+
+        return res;
     }
 }
